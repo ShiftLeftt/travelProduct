f17c0b36efb68577d74d2d6fdaac095d85d3b786
김재승 <jaes@gmail.com>
2025-02-26 21:10:32 +0900

    commit

--- travel/src/App.js
+++ travel/src/App.js
@@ -1,19 +1,21 @@
 // import React, { createContext, useContext } from 'react';
 
 import LeftSide from "./pages/main/LeftSide";
+import RightSide from "./pages/main/RightSide";
 import Main from "./pages/main/Main";
-import style from "../public/css/app.module.css"
+import styles from "./app.module.css"
 
-const displayFlex = {
-  display: "flex", 
-  alignItems: "center",
-  height:"100%"
-}
+// const displayFlex = {
+//   display: "flex", 
+//   alignItems: "center",
+//   height:"100%"
+// }
 function App() {
   return (
-    <div style={displayFlex}> {/* common.css로 수정해야함 */}
+    <div className={styles.displayFlex}> {/* common.css로 수정해야함 */}
       <LeftSide /> 
       <Main /> 
+      <RightSide/>
     </div>
   );
 }
--- travel/src/pages/main/rightSide.js
+++ travel/src/pages/main/rightSide.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import style from './rightSide.module.css'; // 상대 경로로 수정
 // import style from "main.module.css";
 function RightSide() {
   return (
-    <div className='right-side-bar'>
+    <div className={style.rightSideBar}>
       <img src="" alt="" />
       <img src="" alt="" />
     </div>
