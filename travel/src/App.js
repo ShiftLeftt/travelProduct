ba118279d12636388865fa19f1d5548de34694e8
김재승 <jaes@gmail.com>
2025-02-27 15:54:36 +0900

    메인 라우팅 진행

--- travel/src/App.js
+++ travel/src/App.js
@@ -5,14 +5,8 @@ import RightSide from "./pages/main/RightSide";
 import Main from "./pages/main/Main";
 import styles from "./app.module.css"
 import './styles/common.css'
-// import './styles/fonts.css'
+import './styles/fonts.css'
 import './styles/reset.css'
-
-// const displayFlex = {
-//   display: "flex", 
-//   alignItems: "center",
-//   height:"100%"
-// }
 function App() {
   return (
     <div className={styles.displayFlex}> {/* common.css로 수정해야함 */}
