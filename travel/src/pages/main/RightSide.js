9a3af509273518b73c4a381eb9eadfd62c6785e0
JadeNmeister <bigdragon00@naver.com>
2025-03-04 20:42:06 +0900

    사이드바 애니메이션 조절

--- travel/src/pages/main/RightSide.js
+++ travel/src/pages/main/RightSide.js
@@ -4,10 +4,17 @@ import "../../styles/common.css"; // 상대 경로로 수정
 import "../../styles/reset.css";
 
 function RightSide() {
+
+    const [isClicked, setIsClicked] = React.useState(false);
+
+    const toggle = () =>{
+        setIsClicked(!isClicked);
+    }
   return (
-    <div className={style.rightSideBar}>
-      <button>
-        <img src="/img/leftArrow.svg" alt="왼쪽방향 화살표" />
+    <div className={isClicked ? `${style.rightSideBar} ${style.clicked}` : style.rightSideBar }>
+      <button onClick={toggle} className={style.rightSideButton}>
+          {isClicked && <img src="/img/rightArrow.svg" alt="오른쪽방향 화살표" />}
+          {!isClicked && <img src="/img/leftArrow.svg" alt="왼쪽방향 화살표" />}
       </button>
       <div className={style.logWrap}>
         <img src="/img/Journee_logo.webp" alt="" />
