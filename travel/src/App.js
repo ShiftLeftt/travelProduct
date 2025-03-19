b89ab4c222eb0935cb7f174060f1d46624fbfc65
김재승 <jaes@gmail.com>
2025-03-19 21:28:39 +0900

    test

--- travel/src/App.js
+++ travel/src/App.js
@@ -3,18 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Seasons from "./pages/seasons/seasons";
 import Community from "./pages/community/community";
 import Random from "./pages/random/random";
-// import TravelPlan from "./pages/main/TravelPlan";
-// import UserLogin from "./pages/main/UserLogin";
+import TravelPlan from "./pages/main/TravelPlan";
+import UserLogin from "./pages/main/UserLogin";
 import Main from "./pages/main/Main";
-import RightSide from "./pages/main/RightSide";
-import LeftSide from "./pages/main/LeftSide";
-// <<<<<<< HEAD
+// import RightSide from "./pages/main/RightSide";
+// import LeftSide from "./pages/main/LeftSide";
+
 import Home from "./pages/home/home";
 import styles from "./app.module.css";
 import "./styles/common.css";
 
 import "./styles/reset.css";
-// * =======
+
 // import Home from "./pages/home/home"
 // import styles from "./app.module.css"
 // import './styles/common.css'
@@ -39,7 +39,7 @@ function App() {
                 
                 <UserLogin/>
 ======= */}
-        <LeftSide />
+        <TravelPlan />
         <Main>
           <Routes>
             <Route path="/community" element={<Community />} />
@@ -48,7 +48,7 @@ function App() {
             <Route path="/home" element={<Home />} />
           </Routes>
         </Main>
-        <RightSide />
+        <UserLogin />
         {/* >>>>>>> origin/signupTask */}
       </div>
     </Router>
--- travel/src/pages/main/UserLogin.js
+++ travel/src/pages/main/UserLogin.js
@@ -1,4 +1,4 @@
-import React, { useState }  from "react";
+import React, { useState } from "react";
 import style from "./userLogin.module.css"; // CSS 모듈 import
 import "../../styles/common.css"; // 상대 경로로 수정
 import "../../styles/reset.css";
@@ -10,37 +10,46 @@ function UserLogin() {
   const [show, setShow] = useState(false);
 
   const kakao = () => {
-    window.location.href = "http://34.47.116.81:8080/oauth2/authorization/kakao?redirect_uri=http://localhost:3000/loginSuccess";
-  }
+    window.location.href =
+      "http://34.47.116.81:8080/oauth2/authorization/kakao?redirect_uri=http://localhost:3000/loginSuccess";
+  };
 
-  const handleClick =()=>{
+  const handleClick = () => {
     setIsExpanded(!isExpanded);
     setIsVisible(false);
-  }
+  };
   return (
-    <div className={`${style.userLoginBar} ${isExpanded ? style.isExpanded: ""}`}>
-
+    <div
+      className={`${style.userLoginBar} ${isExpanded ? style.isExpanded : ""}`}
+    >
       <button onClick={handleClick} className={style.toggleButton}>
-        <img src="/img/leftArrow.svg" alt="왼쪽방향 화살표" className={isExpanded ? style.rotated:""}/>
+        <img
+          src="/img/leftArrow.svg"
+          alt="왼쪽방향 화살표"
+          className={isExpanded ? style.rotated : ""}
+        />
       </button>
 
-        <div className={style.logWrap}>
-          <img src="/img/Journee_logo.webp" alt="Journee" />
-        </div>
+      <div className={style.logWrap}>
+        <img src="/img/Journee_logo.webp" alt="Journee" />
+      </div>
       {isExpanded && (
         <div className={style.loginArea}>
           <h2>로그인</h2>
           <div className={style.loginInput}>
             <input type="text" placeholder="아이디를 입력해주세요" />
-            <input type="password" placeholder="비밀번호를 입력해주세요"/>
+            <input type="password" placeholder="비밀번호를 입력해주세요" />
           </div>
           <div className={style.authButtons}>
             <button className={style.loginBtn}>
               <img src="/img/loginBtn01.png" alt="LOGIN" />
               LOGIN
             </button>
-            <button onClick={()=>setShow(true)} className={style.signupBtn}
-                    {show && <SignupModal/>}>
+            <button
+              onClick={() => setShow(true)}
+              className={style.signupBtn}
+              {...(show && <SignupModal />)}
+            >
               <img src="/img/loginBtn02.png" alt="SIGN UP" />
               SIGN UP
             </button>
@@ -51,7 +60,8 @@ function UserLogin() {
             <img src="/img/google.png" alt="구글로 로그인" />
           </div>
           <p>
-            회원가입 없이 이용 가능하며 첫 로그인시 이용약관 및 개인정보처리방침 동의로 간주됩니다.
+            회원가입 없이 이용 가능하며 첫 로그인시 이용약관 및 개인정보처리방침
+            동의로 간주됩니다.
           </p>
           <div className={style.loginAd}>
             <img src="/img/loginAd01.png" alt="광고" />
