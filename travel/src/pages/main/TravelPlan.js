4d32ce01ff4ff6e5dbbb7baae6e376841097c10a
Jeon-ChaeA <ahwon1004@gmail.com>
2025-03-24 11:03:55 +0900

    locationBox 영역 생성

--- travel/src/pages/main/TravelPlan.js
+++ travel/src/pages/main/TravelPlan.js
@@ -1,15 +1,19 @@
-import React from "react";
-import style from "./travelPlan.module.css"; // CSS 모듈 import
-import "../../styles/common.css"; // 상대 경로로 수정
-import "../../styles/reset.css"; // 상대 경로로 수정
+import React, { useState } from "react";
+import style from "./travelPlan.module.css";
+import "../../styles/common.css";
+import "../../styles/reset.css";
 
 function TravelPlan() {
+  const [isBoxVisible, setIsBoxVisible] = useState(false);
+
+  const toggleBox = () => {
+    setIsBoxVisible((prev) => !prev);
+  };
+
   return (
-    <div className={style.travelPlan}>
+    <div className={style.travelPlan} style={{ position: "relative" }}>
       <div>
         <div className={style.travelPlanTitle}>
-          {" "}
-          {/* 중괄호로 수정 */}
           <p>
             고객님, <br />
             <span>
@@ -17,22 +21,40 @@ function TravelPlan() {
               계획하시나요?
             </span>
           </p>
-          <button>
-            <img src="/img/rightArrow.svg" alt="" />
+          <button onClick={toggleBox}>
+            <img
+              src={isBoxVisible ? "/img/leftArrow.svg" : "/img/rightArrow.svg"}
+              alt="arrow icon"
+            />
           </button>
         </div>
+
+        <div
+          className={`${style.locationBox} ${
+            isBoxVisible ? style.locationBoxVisible : ""
+          }`}
+        >
+          <p>
+            내용 입력예정
+          </p>
+        </div>
+
         <div className={style.travelPlanMainBtn}>
           <button>여행 계획 시작하기</button>
         </div>
+
         <div className={style.liveSearch}>
           <h3>실시간 인기검색어</h3>
           <ul>
             <li>1. 삼척정월대보름제</li>
-            <li>2. 삼척정월대보름제</li>
-            <li>3. 삼척정월대보름제</li>
+            <li>2. 태백겨울축제</li>
+            <li>3. 논산 딸기 축제</li>
+            <li>4. 청평 설빙 송어빙어 축제</li>
+            <li>5. 별빛이 흐르는 정원</li>
           </ul>
         </div>
       </div>
+
       <div className={style.bannerImgWrap}>
         <img src="/img/Journee_banner01.webp" alt="" />
         <img src="/img/Journee_banner02.webp" alt="" />
