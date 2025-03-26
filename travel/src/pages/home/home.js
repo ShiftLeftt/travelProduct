6799dbf07321a001e045178b0d27b430c2bb9cf7
김재승 <jaes@gmail.com>
2025-03-26 18:42:19 +0900

    commit

--- travel/src/pages/home/home.js
+++ travel/src/pages/home/home.js
@@ -1,11 +1,9 @@
 function Home() {
-    return (
-        <div style={{ padding: '20px', textAlign: 'center' }}>
-            <h1>메인!</h1>
-
-
-        </div>
-    );
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>메인!</h1>
+    </div>
+  );
 }
 
 export default Home;
