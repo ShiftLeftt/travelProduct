a766691ec5d423030366075f0558fefd83a5a230
최정민 <bigdragon00@naver.com>
2025-04-02 19:24:29 +0900

    token 넘겨주는 로직 작성중

---
+++ travel/src/api/axiosInstance.js
@@ -0,0 +1,7 @@
+instance.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
