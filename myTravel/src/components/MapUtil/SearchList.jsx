9e043a77d2543c8a2d959d515521772fcba3bc47
Jadenmeister <bigdragon00@naver.com>
2025-05-13 00:33:10 +0900

    feat: 검색 리스트의 값을 저장해서 온클릭 이벤트에 툴팁 뜨도록 구현 (수정 필요)

--- myTravel/src/components/MapUtil/SearchList.jsx
+++ myTravel/src/components/MapUtil/SearchList.jsx
@@ -1,10 +1,12 @@
 import styles from './SearchList.module.css';
+import { useContext } from 'react';
+import {LocationContext} from '../../contexts/LocationContext.jsx'
 
 
-
-export default function SearchList({ places,onItemHover, pagination, onAdd, onPageChange}) {
+export default function SearchList({ places,onItemHover, pagination, onAdd, onPageChange, markers}) {
 
   const noResults = !pagination && places.length === 0;
+  const {setFocusMarker,setCenter,setZoomLevel,setInfoIndex} = useContext(LocationContext);
 
     return (
 
@@ -20,6 +22,12 @@ export default function SearchList({ places,onItemHover, pagination, onAdd, onPa
                           className={styles.item}
                           onMouseOver={() => onItemHover(p, i)}
                           onMouseOut={() => onItemHover(null, null)}
+                          // 클릭된 요소 저장
+                          onClick={() => {
+                            const markerIndex = markers.findIndex(m => m.title === p.place_name);
+                            setFocusMarker({ index: markerIndex, marker: p }); // 객체 저장
+                            onItemHover(p, markerIndex);
+                          }}
                       >
                         <div className={styles.info}>
                         <strong>{p.place_name}</strong><br/>
