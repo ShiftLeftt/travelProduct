cf77045797f7fd102863d56be7227e9f49483e01
JadenMeister <bigdragon00@naver.com>
2025-05-14 20:44:59 +0900

    feat: 마커 누르면 툴팁 동작 하긴 하지만 동작 안되는 마커 있음 수정 필요

--- myTravel/src/components/MapUtil/Map.jsx
+++ myTravel/src/components/MapUtil/Map.jsx
@@ -1,20 +1,19 @@
 
 import React, { useEffect, useState, useContext } from 'react';
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { Map, MapMarker, MapInfoWindow as InfoWindow  } from 'react-kakao-maps-sdk';
 import { LocationContext } from '../../contexts/LocationContext';
 import {regionOfficeMap} from "../../functions/regionCities.js";
 
 
 export default function KakaoMap() {
-    const { selectedRegion, selectedCity, searchKeyword, map,setMap, center, setCenter,infoIndex,setInfoIndex, setZoomLevel, zoomLevel } = useContext(LocationContext);
+    const { selectedRegion, selectedCity, searchKeyword, map,setMap, center, setCenter, setZoomLevel, zoomLevel } = useContext(LocationContext);
 
-
+    const [infoIndex, setInfoIndex] = useState(null);
     const [mapLoad, setMapLoad] = useState(false);
     const [cityBounds, setCityBounds] = useState(null);
     const [page, setPage] = useState(1);
-    const [searchResults, setSearchResults] = useState([]);
-    const [searchPagination, setSearchPagination] = useState(null);
-    const {markers, setMarkers} = useContext(LocationContext);
+    const [markers, setMarkers] = useState([]);
+
 
 
     // SearchList에서 클릭한 요소, 검색 리스트 클릭이벤트 상태값
@@ -61,7 +60,7 @@ export default function KakaoMap() {
                     return place.address_name.includes(selectedCity) ||
                     place.road_address_name.includes(selectedRegion);
                 })
-                const newMarkers = data.map(p => {
+                const newMarkers = filteredData.map(p => {
                     const lat = parseFloat(p.y);
                     const lng = parseFloat(p.x);
                     bounds.extend(new window.kakao.maps.LatLng(lat, lng));
@@ -72,11 +71,10 @@ export default function KakaoMap() {
                 if(newMarkers.length > 0 ){
                     map.setBounds(bounds);
                 }
-                map.setBounds(bounds);
             } else {
                 setMarkers([]);
             }
-        }, { bounds: cityBounds, page });
+        },);
 
     }, [searchKeyword, mapLoad, map, selectedCity, selectedRegion, cityBounds]);
 
@@ -87,27 +85,35 @@ export default function KakaoMap() {
         <Map center={center}  level={zoomLevel} style={{ flex: 1, height: '100%' }}onCreate={mapInstanc => {setMap(mapInstanc)} }>
             <MapMarker position={center} />
             {markers.map((m, i) => (
-                <MapMarker key={i} position={m.position}
+              <React.Fragment key={i}>
+
+              <MapMarker position={m.position}
                            title={m.title}
+                           clickable={true}
                            //2. 클릭이벤트에 해당 마커에 대한 줌 주고 정보 툴팁 띄우기 줌 레벨은 작을수록 가까이 보임
                            onClick={() => {
                                 setInfoIndex(i);
-                                setZoomLevel(1);
+                                setZoomLevel(3);
                                 setCenter(m.position);
-                           }}>
-                    {infoIndex === i || focusMarker === i ? (
-                        <div style={{ background: '#fff', padding: '5px', borderRadius: '5px', whiteSpace: 'pre-line', fontSize: '12px', maxWidth:`20rem` }}>
-                            <h4>{m.title}</h4>
-                            <p>{m.place.address_name}</p>
-                            <p>{m.place.phone || "번호 없음"}</p>
-                            <p>{m.place.road_address_name}</p>
-                        </div>
-                    ) : null}
-
-
-
-                </MapMarker>
-
+                           }}/>
+                    {(infoIndex === i || focusMarker === i) && (
+                      <InfoWindow
+                        position={m.position}
+                        removable={true}                      // 닫기 버튼 표시
+                        onCloseClick={() => setInfoIndex(null)}
+                        options={{
+                            offset: new window.kakao.maps.Point(0, -100),
+                        }}
+                      >
+                          <div style={{ padding: '5px', fontSize: '12px' }}>
+                              <h4>{m.title}</h4>
+                              <p>{m.place.address_name}</p>
+                              <p>{m.place.phone || "번호 없음"}</p>
+                              <p>{m.place.road_address_name}</p>
+                          </div>
+                      </InfoWindow>
+                        )}
+              </React.Fragment>
             ))}
         </Map>
     );
